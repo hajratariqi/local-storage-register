be0af84fe4886cb1ade8c306533f20c9be4dda98
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-20 13:11:16 +0500

    added form validation

--- register.js
+++ register.js
@@ -1,10 +1,15 @@
 const register = () =>{
+    let nameRegex = /^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$/;
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
+
     let saveDate = localStorage.getItem('user')
     let oldUser = saveDate ? JSON.parse(saveDate) : []
-    if(name && email && password){
+
+    if(nameRegex.test(name) && emailRegex.test(email) && password){
         const newUser = {name: name,  email: email,  password: password};
         oldUser.push(newUser)
         localStorage.setItem("user", JSON.stringify(oldUser))
